3235f67c7cfaa9629247ed06521231b7f474d021
Oleg Petrakov <o.petrakov@bi.zone>
2020-03-19 00:28:47 +0300

    Выделили PackageID пока в MainActivity. Потом буду брать из build.gradle

--- app/src/main/java/com/zfr/ctfzoneclient/MainActivity.kt
+++ app/src/main/java/com/zfr/ctfzoneclient/MainActivity.kt
@@ -3,6 +3,8 @@ package com.zfr.ctfzoneclient
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+const val PACKAGE_ID = "com.zfr.ctfzoneclient"
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
