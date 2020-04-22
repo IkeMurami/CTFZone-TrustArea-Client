69c888f6bd93cfe44d7af4105ff6fc5adba87afd
Oleg Petrakov <o.petrakov@bi.zone>
2020-04-22 09:49:37 +0300

    Fix bugs

--- app/src/main/java/ctfz/trustarea/client/database/tables/SessionDao.kt
+++ app/src/main/java/ctfz/trustarea/client/database/tables/SessionDao.kt
@@ -9,7 +9,7 @@ interface SessionDao {
     @Query("SELECT * FROM TokenDBEntity WHERE token LIKE :token LIMIT 1")
     fun userByToken(token: String): TokenDBEntity?
 
-    @Query("SELECT * FROM TokenDBEntity WHERE username LIKE :username LIMIT 1")
+    @Query("SELECT * FROM TokenDBEntity WHERE username LIKE :username")
     fun userSessions(username: String): List<TokenDBEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
